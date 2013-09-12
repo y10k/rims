26cd87e3c1a846eed2121ca24f2fbcea3a2de8b6
TOKI Yoshinori <toki@freedom.ne.jp>
2013-09-12 11:20:00 +0900

    mailbox db unit test.

--- test/test_db.rb
+++ test/test_db.rb
@@ -86,6 +86,56 @@ module RIMS::Test
       assert_equal([ 1 ].to_set, @msg_db.msg_mboxes(0))
     end
   end
+
+  class MailboxDBTest < Test::Unit::TestCase
+    def setup
+      @kv_store = {}
+      @mbox_db = RIMS::MailboxDB.new(RIMS::GDBM_KeyValueStore.new(@kv_store))
+    end
+
+    def test_attributes
+      @mbox_db.mbox_id = 0
+      assert_equal(0, @mbox_db.mbox_id)
+      assert_equal('0', @kv_store['mbox_id'])
+
+      @mbox_db.mbox_name = 'INBOX'
+      assert_equal('INBOX', @mbox_db.mbox_name)
+      assert_equal('INBOX', @kv_store['mbox_name'])
+    end
+
+    def test_counter
+      assert_equal(0, @mbox_db.msgs)
+      @mbox_db.msgs_increment
+      assert_equal(1, @mbox_db.msgs)
+      assert_equal('1', @kv_store['msg_count'])
+      @mbox_db.msgs_decrement
+      assert_equal(0, @mbox_db.msgs)
+      assert_equal('0', @kv_store['msg_count'])
+
+      for n in %w[ seen answered flagged deleted draft recent ]
+        assert_equal(0, @mbox_db.flags(n), "flag_#{n}")
+        @mbox_db.flags_increment(n)
+        assert_equal(1, @mbox_db.flags(n), "flag_#{n}")
+        assert_equal('1', @kv_store["flags_#{n}"])
+        @mbox_db.flags_decrement(n)
+        assert_equal(0, @mbox_db.flags(n), "flag_#{n}")
+        assert_equal('0', @kv_store["flags_#{n}"])
+      end
+    end
+
+    def test_msg
+      @mbox_db.add_msg(0)
+      assert_equal([ 0 ], @mbox_db.each_msg_id.to_a)
+      assert_equal(false, @mbox_db.msg_flag_del(0))
+      assert_equal({ 'msg-0' => '' }, @kv_store)
+      @mbox_db.set_msg_flag_del(0, true)
+      assert_equal(true, @mbox_db.msg_flag_del(0))
+      assert_equal({ 'msg-0' => 'deleted' }, @kv_store)
+      @mbox_db.expunge_msg(0)
+      assert_equal([], @mbox_db.each_msg_id.to_a)
+      assert_equal({}, @kv_store)
+    end
+  end
 end
 
 # Local Variables:
