ea426d96aec855359a94cad944892cbf610dd913
TOKI Yoshinori <toki@freedom.ne.jp>
2013-09-16 13:25:14 +0900

    mailbox API.

--- lib/rims/mail_store.rb
+++ lib/rims/mail_store.rb
@@ -41,6 +41,57 @@ module RIMS
       @global_db.sync
       self
     end
+
+    def cnum
+      @global_db.cnum
+    end
+
+    def uid
+      @global_db.uid
+    end
+
+    def uidvalidity
+      @global_db.uidvalidity
+    end
+
+    def add_mbox(name)
+      if (@global_db.mbox_id(name)) then
+        raise "duplicated mailbox name: #{name}."
+      end
+
+      cnum = @global_db.cnum
+      next_id = @global_db.uidvalidity
+
+      @mbox_db[next_id] = MessageDB.new(open_kvs("mbox_#{next_id}.db"))
+      @mbox_db[next_id].mbox_id = next_id
+      @mbox_db[next_id].mbox_name = name
+
+      @global_db.uidvalidity = next_id + 1
+      @global_db.add_mbox(next_id, name)
+      @global_db.cnum = cnum + 1
+
+      next_id
+    end
+
+    def del_mbox(id)
+      cnum = @global_db.cnum
+
+      mbox_db = @mbox_db[id] or raise "not found a mailbox: #{id}."
+      mbox_db.each_msg_id do |msg_id|
+        @msg_db.del_msg_mbox(msg_id, id)
+      end
+      mbox_db.close
+
+      name = @global_db.del_mbox(id) or raise "internal error: not found a mailbox: #{id}"
+      @global_db.cnum = cnum + 1
+
+      name
+    end
+
+    def each_mbox_id(*args, &block)
+      @global_db.each_mbox_id(*args, &block)
+      self
+    end
   end
 end
 
