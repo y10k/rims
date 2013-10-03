ee6da4e2d65d30adf157fc214cb5d9165a4b81e8
TOKI Yoshinori <toki@freedom.ne.jp>
2013-10-03 22:14:29 +0900

    unit test of protocol decoder. lsub is not implemented.

--- test/test_protocol.rb
+++ test/test_protocol.rb
@@ -506,6 +506,26 @@ Hello Joe, do you think we can meet at 3:30 tomorrow?
       assert_raise(StopIteration) { res.next }
     end
 
+    def test_lsub_not_implemented
+      assert_equal(false, @decoder.auth?)
+
+      res = @decoder.lsub('T001', '', '').each
+      assert_match(/^T001 NO /, res.next)
+      assert_raise(StopIteration) { res.next }
+
+      assert_equal(false, @decoder.auth?)
+
+      res = @decoder.login('T002', 'foo', 'open_sesame').each
+      assert_equal('T002 OK LOGIN completed', res.next)
+      assert_raise(StopIteration) { res.next }
+
+      assert_equal(true, @decoder.auth?)
+
+      res = @decoder.lsub('T003', '', '').each
+      assert_equal('T003 BAD not implemented', res.next)
+      assert_raise(StopIteration) { res.next }
+    end
+
     def test_command_loop_capability
       output = StringIO.new('', 'w')
       input = StringIO.new(<<-'EOF', 'r')
