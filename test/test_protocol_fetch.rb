7cf8d65763086799f458383914bb05bba90c16a7
TOKI Yoshinori <toki@freedom.ne.jp>
2013-12-11 17:47:03 +0900

    fetch attribute parser. refactored unit test.

--- test/test_protocol_fetch.rb
+++ test/test_protocol_fetch.rb
@@ -20,7 +20,16 @@ module RIMS::Test
       @mail_store = RIMS::MailStore.new(@kvs_open, @kvs_open)
       @mail_store.open
       @inbox_id = @mail_store.add_mbox('INBOX')
+    end
 
+    def make_fetch_parser
+      yield if block_given?
+      @folder = @mail_store.select_mbox(@inbox_id)
+      @parser = RIMS::Protocol::FetchParser.new(@mail_store, @folder)
+    end
+    private :make_fetch_parser
+
+    def add_mail_simple
       @mail_store.add_msg(@inbox_id, <<-'EOF', Time.parse('2013-11-08 06:47:50 +0900'))
 To: foo@nonet.org
 From: bar@nonet.org
@@ -32,7 +41,10 @@ Date: Fri,  8 Nov 2013 06:47:50 +0900 (JST)
 
 Hello world.
       EOF
+    end
+    private :add_mail_simple
 
+    def add_mail_multipart
       @mail_store.add_msg(@inbox_id, <<-'EOF', Time.parse('2013-11-08 19:31:03 +0900'))
 To: bar@nonet.com
 From: foo@nonet.com
@@ -106,7 +118,10 @@ Content-Type: text/html; charset=us-ascii
 --1383.905529.351299--
 --1383.905529.351297--
       EOF
+    end
+    private :add_mail_multipart
 
+    def add_mail_mime_subject
       @mail_store.add_msg(@inbox_id, <<-'EOF', Time.parse('2013-11-08 19:31:03 +0900'))
 Date: Fri, 8 Nov 2013 19:31:03 +0900
 Subject: =?ISO-2022-JP?B?GyRCJEYkOSRIGyhC?=
@@ -121,12 +136,14 @@ Message-Id: <20131107214750.445A1255B9F@smtp.nonet.com>
 
 Hello world.
       EOF
-
-      @folder = @mail_store.select_mbox(@inbox_id)
-      @parser = RIMS::Protocol::FetchParser.new(@mail_store, @folder)
     end
+    private :add_mail_mime_subject
 
     def test_parse_bodystructure
+      make_fetch_parser{
+        add_mail_simple
+        add_mail_multipart
+      }
       fetch = @parser.parse('BODYSTRUCTURE')
       assert_equal('BODYSTRUCTURE ' +
                    encode_list([ 'TEXT',
@@ -182,6 +199,11 @@ Hello world.
     end
 
     def test_parse_envelope
+      make_fetch_parser{
+        add_mail_simple
+        add_mail_multipart
+        add_mail_mime_subject
+      }
       fetch = @parser.parse('ENVELOPE')
       assert_equal('ENVELOPE (' + [
                      '"Fri, 08 Nov 2013 06:47:50 +0900"',       # Date
@@ -225,18 +247,30 @@ Hello world.
     end
 
     def test_parse_internaldate
+      make_fetch_parser{
+        add_mail_simple
+        add_mail_multipart
+      }
       fetch = @parser.parse('INTERNALDATE')
       assert_equal('INTERNALDATE "08-11-2013 06:47:50 +0900"', fetch.call(@folder.msg_list[0]))
       assert_equal('INTERNALDATE "08-11-2013 19:31:03 +0900"', fetch.call(@folder.msg_list[1]))
     end
 
     def test_parse_uid
+      make_fetch_parser{
+        add_mail_simple
+        add_mail_multipart
+      } 
       fetch = @parser.parse('UID')
       assert_equal('UID 1', fetch.call(@folder.msg_list[0]))
       assert_equal('UID 2', fetch.call(@folder.msg_list[1]))
     end
 
     def test_parse_group_empty
+      make_fetch_parser{
+        add_mail_simple
+        add_mail_multipart
+      } 
       fetch = @parser.parse([ :group ])
       assert_equal('()', fetch.call(@folder.msg_list[0]))
       assert_equal('()', fetch.call(@folder.msg_list[1]))
