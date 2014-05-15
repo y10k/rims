1a08bc1c94ff0b2f18ace0a0df556be5b0fac254
TOKI Yoshinori <toki@freedom.ne.jp>
2014-05-15 19:40:57 +0900

    refactored unit test: test_protocol_search.rb test_parse_flagged

--- test/test_protocol_search.rb
+++ test/test_protocol_search.rb
@@ -245,16 +245,18 @@ Content-Type: text/html
 
     def test_parse_flagged
       make_search_parser{
-        @mail_store.add_msg(@inbox_id, 'foo')
-        @mail_store.add_msg(@inbox_id, 'foo')
-        assert_equal([ 1, 2 ], @mail_store.each_msg_uid(@inbox_id).to_a)
-        @mail_store.set_msg_flag(@inbox_id, 1, 'flagged', true)
-        assert_equal(true, @mail_store.msg_flag(@inbox_id, 1, 'flagged'))
-        assert_equal(false, @mail_store.msg_flag(@inbox_id, 2, 'flagged'))
+        add_msg('foo')
+        add_msg('foo')
+        assert_msg_uid(1, 2)
+
+        set_msg_flag(1, 'flagged', true)
+        assert_msg_flag('flagged', true, false)
       }
-      cond = @parser.parse([ 'FLAGGED' ])
-      assert_equal(true, cond.call(@folder.msg_list[0]))
-      assert_equal(false, cond.call(@folder.msg_list[1]))
+
+      parse_search_key([ 'FLAGGED' ]) {
+        assert_search_cond(0, true)
+        assert_search_cond(1, false)
+      }
     end
 
     def test_parse_from
