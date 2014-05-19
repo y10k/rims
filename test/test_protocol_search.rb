9730421f14d37cca577722d528ecf4774b883dfd
TOKI Yoshinori <toki@freedom.ne.jp>
2014-05-19 11:50:21 +0900

    refactored unit test: test_protocol_search.rb test_parse_since

--- test/test_protocol_search.rb
+++ test/test_protocol_search.rb
@@ -582,27 +582,21 @@ Content-Type: text/html
 
     def test_parse_since
       make_search_parser{
-        @mail_store.add_msg(@inbox_id, 'foo', Time.parse('2013-11-07 12:34:56'))
-        @mail_store.add_msg(@inbox_id, 'foo', Time.parse('2013-11-08 12:34:56'))
-        @mail_store.add_msg(@inbox_id, 'foo', Time.parse('2013-11-09 12:34:56'))
-        assert_equal([ 1, 2, 3 ], @mail_store.each_msg_uid(@inbox_id).to_a)
-        assert_equal(Time.parse('2013-11-07 12:34:56'), @mail_store.msg_date(@inbox_id, 1))
-        assert_equal(Time.parse('2013-11-08 12:34:56'), @mail_store.msg_date(@inbox_id, 2))
-        assert_equal(Time.parse('2013-11-09 12:34:56'), @mail_store.msg_date(@inbox_id, 3))
+        add_msg('foo', Time.parse('2013-11-07 12:34:56'))
+        add_msg('foo', Time.parse('2013-11-08 12:34:56'))
+        add_msg('foo', Time.parse('2013-11-09 12:34:56'))
+        assert_msg_uid(1, 2, 3)
       }
-      cond = @parser.parse([ 'SINCE', '08-Nov-2013' ])
-      assert_equal(false, cond.call(@folder.msg_list[0]))
-      assert_equal(false, cond.call(@folder.msg_list[1]))
-      assert_equal(true, cond.call(@folder.msg_list[2]))
-      assert_raise(RIMS::SyntaxError) {
-        @parser.parse([ 'SINCE' ])
+
+      parse_search_key([ 'SINCE', '08-Nov-2013' ]) {
+        assert_search_cond(0, false)
+        assert_search_cond(1, false)
+        assert_search_cond(2, true)
       }
-      assert_raise(RIMS::SyntaxError) {
-        @parser.parse([ 'SINCE', '99-Nov-2013' ])
-      }
-      assert_raise(RIMS::SyntaxError) {
-        @parser.parse([ 'SINCE', [ :group, '08-Nov-2013'] ])
-      }
+
+      assert_search_syntax_error([ 'SINCE' ])
+      assert_search_syntax_error([ 'SINCE', '99-Nov-2013' ])
+      assert_search_syntax_error([ 'SINCE', [ :group, '08-Nov-2013'] ])
     end
 
     def test_parse_smaller
