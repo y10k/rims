028e88803367d49618bb6468bbc6495430bf5c30
TOKI Yoshinori <toki@freedom.ne.jp>
2014-05-19 20:29:48 +0900

    refactored unit test: test_protocol_search.rb test_parse_msg_set

--- test/test_protocol_search.rb
+++ test/test_protocol_search.rb
@@ -810,25 +810,23 @@ Content-Type: text/html
 
     def test_parse_msg_set
       make_search_parser{
-        @mail_store.add_msg(@inbox_id, 'foo')
-        @mail_store.add_msg(@inbox_id, 'foo')
-        @mail_store.add_msg(@inbox_id, 'foo')
-        @mail_store.add_msg(@inbox_id, 'foo')
-        @mail_store.add_msg(@inbox_id, 'foo')
-        @mail_store.add_msg(@inbox_id, 'foo')
-        @mail_store.set_msg_flag(@inbox_id, 1, 'deleted', true)
-        @mail_store.set_msg_flag(@inbox_id, 3, 'deleted', true)
-        @mail_store.set_msg_flag(@inbox_id, 5, 'deleted', true)
-        @mail_store.expunge_mbox(@inbox_id)
-        assert_equal([ 2, 4, 6 ], @mail_store.each_msg_uid(@inbox_id).to_a)
+        add_msg('foo')
+        add_msg('foo')
+        add_msg('foo')
+        add_msg('foo')
+        add_msg('foo')
+        add_msg('foo')
+        expunge(1, 3, 5)
+        assert_msg_uid(2, 4, 6)
       }
-      cond = @parser.parse([ '1,*' ])
-      assert_equal(true, cond.call(@folder.msg_list[0]))
-      assert_equal(false, cond.call(@folder.msg_list[1]))
-      assert_equal(true, cond.call(@folder.msg_list[2]))
-      assert_raise(RIMS::SyntaxError) {
-        @parser.parse([ 'detarame' ])
+
+      parse_search_key([ '1,*' ]) {
+        assert_search_cond(0, true)
+        assert_search_cond(1, false)
+        assert_search_cond(2, true)
       }
+
+      assert_search_syntax_error([ 'detarame' ])
     end
 
     def test_parse_group
