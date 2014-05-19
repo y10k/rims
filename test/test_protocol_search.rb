f2f8d0971777f45f90eea12453295e0182f9b0cf
TOKI Yoshinori <toki@freedom.ne.jp>
2014-05-19 20:19:15 +0900

    refactored unit test: test_protocol_search.rb test_parse_undraft

--- test/test_protocol_search.rb
+++ test/test_protocol_search.rb
@@ -748,16 +748,18 @@ Content-Type: text/html
 
     def test_parse_undraft
       make_search_parser{
-        @mail_store.add_msg(@inbox_id, 'foo')
-        @mail_store.add_msg(@inbox_id, 'foo')
-        assert_equal([ 1, 2 ], @mail_store.each_msg_uid(@inbox_id).to_a)
-        @mail_store.set_msg_flag(@inbox_id, 1, 'draft', true)
-        assert_equal(true, @mail_store.msg_flag(@inbox_id, 1, 'draft'))
-        assert_equal(false, @mail_store.msg_flag(@inbox_id, 2, 'draft'))
+        add_msg('foo')
+        add_msg('foo')
+        assert_msg_uid(1, 2)
+
+        set_msg_flag(1, 'draft', true)
+        assert_msg_flag('draft', true, false)
       }
-      cond = @parser.parse([ 'UNDRAFT' ])
-      assert_equal(false, cond.call(@folder.msg_list[0]))
-      assert_equal(true, cond.call(@folder.msg_list[1]))
+
+      parse_search_key([ 'UNDRAFT' ]) {
+        assert_search_cond(0, false)
+        assert_search_cond(1, true)
+      }
     end
 
     def test_parse_unflagged
