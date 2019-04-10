12d7c9ffac920573b4f969e290fdfdd78c610372
TOKI Yoshinori <toki@freedom.ne.jp>
2019-04-10 15:56:37 +0900

    utility method to trace error chain of `Exception#cause'
    to be used to log all errors that occur one after another in multiple
    stages.

--- lib/rims/error.rb
+++ lib/rims/error.rb
@@ -2,6 +2,17 @@
 
 module RIMS
   class Error < StandardError
+    def self.trace_error_chain(exception)
+      return enum_for(:trace_error_chain, exception) unless block_given?
+
+      while (exception)
+        yield(exception)
+        exception = exception.cause
+      end
+
+      nil
+    end
+
     def self.suppress_2nd_error_at_resource_closing(logger: nil)
       if ($!) then
         begin
--- test/test_error.rb
+++ test/test_error.rb
@@ -23,6 +23,34 @@ module RIMS::Test
       pp @flow_list if $DEBUG
     end
 
+    def test_trace_error_chain
+      exception = assert_raise(RuntimeError) {
+        begin
+          begin
+            begin
+              raise 'error level 0'
+            ensure
+              raise 'error level 1'
+            end
+          ensure
+            raise 'error level 2'
+          end
+        ensure
+          raise 'error level 3'
+        end
+      }
+      assert_equal('error level 3', exception.message)
+
+      errors = RIMS::Error.trace_error_chain(exception).to_a
+      assert_equal(4, errors.length)
+      assert_equal([ RuntimeError ] * 4, errors.map(&:class))
+      assert_equal([ 'error level 3',
+                     'error level 2',
+                     'error level 1',
+                     'error level 0'
+                   ], errors.map(&:message))
+    end
+
     def test_suppress_2nd_error_at_resource_closing_no_error
       begin
         @flow_list << :main
